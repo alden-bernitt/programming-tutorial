33411283ffbd723a3b168e0bdc090d184a58c86d
Alden Bernitt <aldencbernitt@gmail.com>
2025-03-17 15:48:35 -0400

    practice feature added. minor ui updates.

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,15 +1,39 @@
 (() => {
+
+const upArrowClass = 'up-arrow';
+const downArrowClass = 'down-arrow';
+
+function setDropdownIcon(arrowIcon, hidden)
+{
+    arrowIcon.classList.remove(upArrowClass);
+    arrowIcon.classList.remove(downArrowClass);
+
+    const arrowClass = hidden ? downArrowClass : upArrowClass;
+    arrowIcon.classList.add(arrowClass);
+}
+
+// Setup all dropdown sections.
 const ddsections = document.getElementsByClassName('dropdown-section');
 for (const sect of ddsections) {
     const dropdowns = sect.querySelectorAll('& > .dropdown');
     const contents = sect.querySelectorAll('& > .dropdown-contents');
 
-    for (const drops of dropdowns) {
-        drops.addEventListener('click', () => {
+    // Initialize dropdown state
+    for (const drop of dropdowns) {
+        const arrowIcon = drop.querySelector('.arrow-icon')
+        // Hide the dropdown contents if they are not already.
+        for (const c of contents) {
+            c.hidden = true;
+            arrowIcon.classList.add(downArrowClass);   
+        }
+
+       // Reveal the dropdown contents when the dropdown is clicked.
+        drop.addEventListener('click', () => {
             for (const c of contents) {
                 c.hidden = !c.hidden;
+                setDropdownIcon(arrowIcon, c.hidden);
             }
         });
     }
 }
-})()
+})();
---
+++ scripts/practice.js
@@ -0,0 +1,153 @@
+(() => {
+
+const problemAnsweredClass = 'answered';
+const problemCorrectClass = 'correct';
+const problemIncorrectClass = 'incorrect';
+const checkmarkClass = 'checkmark';
+const xmarkClass = 'xmark';
+
+const calcOutputPrompt = 'Determine the output of this code.';
+
+function setProblemPrompt(problemArgs)
+{
+    problemArgs.problemPrompt.textContent =
+            problemArgs.problemData[problemArgs.currProblem].promptText;
+}
+
+function answerIsCorrect(problemArgs)
+{
+    const data = problemArgs.problemData[problemArgs.currProblem];
+    return data.answerInput.value == data.solutionText;
+}
+
+function updateResultUi(problemArgs)
+{
+    const section = problemArgs.section;
+    const marker = problemArgs.resultMarker;
+    const data = problemArgs.problemData[problemArgs.currProblem];
+
+    if (data.answerInput.value === "") {
+        section.classList.remove(problemAnsweredClass);
+        section.classList.remove(problemCorrectClass);
+        section.classList.remove(problemIncorrectClass);
+        marker.classList.remove(checkmarkClass);
+        marker.classList.remove(xmarkClass);
+    }
+    else {
+        section.classList.add(problemAnsweredClass);
+
+        if (answerIsCorrect(problemArgs)) {
+            section.classList.remove(problemIncorrectClass);
+            section.classList.add(problemCorrectClass);
+            marker.classList.remove(xmarkClass);
+            marker.classList.add(checkmarkClass);
+        }
+        else {
+            section.classList.remove(problemCorrectClass);
+            section.classList.add(problemIncorrectClass);
+            marker.classList.remove(checkmarkClass);
+            marker.classList.add(xmarkClass);
+        }
+   }
+}
+
+function updateProblemUi(problemArgs)
+{
+    setProblemPrompt(problemArgs);
+    updateResultUi(problemArgs);
+    problemArgs.currentCounter.textContent = problemArgs.currProblem + 1;
+}
+
+function navigateProblem(problemArgs, offset)
+{
+    const len = problemArgs.problems.length;
+    const curr = problemArgs.currProblem;
+
+    // Normalize offset.
+    offset %= len;
+    
+    problemArgs.problems[curr].hidden = true;
+
+    // Go to the next question, according to offset.
+    // We have to add the problem list length before the modulo operation
+    // to prevent ever performing a modulo on a negative value.
+    const next  = (curr + offset + len) % len;
+    problemArgs.currProblem = next;
+    problemArgs.problems[next].hidden = false;
+
+    updateProblemUi(problemArgs);
+
+    return curr;
+}
+
+function submitAnswer(problemArgs)
+{
+    updateProblemUi(problemArgs);
+
+    if (answerIsCorrect(problemArgs))
+        navigateProblem(problemArgs, 1);
+
+    const curr = problemArgs.currProblem;
+    problemArgs.problemData[curr].answerInput.focus();
+}
+
+// Setup practice sections.
+const psections = document.getElementsByClassName('practice');
+for (const sect of psections) {
+    const problems = sect.querySelectorAll('&> .problem');
+    const problemPrompt = sect.querySelector('.problem-prompt');
+    const currentCounter = sect.querySelector('.current-problem');
+    const totalCounter = sect.querySelector('.total-problems');
+    const resultMarker = sect.querySelector('.answer-marker .marker');
+    const prevButton = sect.querySelector('.prev-button');
+    const nextButton = sect.querySelector('.next-button');
+    const checkButton = sect.querySelector('.check-button');
+
+    // Cache problem data.
+    const problemData = [];
+    for (const prob of problems) {
+        problemData.push({
+            promptText: prob.querySelector('.problem-prompt-text').value,
+            solutionText: prob.querySelector('.problem-solution').value,
+            answerInput: prob.querySelector('.problem-answer')
+        });
+    }
+
+    const problemArgs = {
+        section: sect,
+        problems: problems,
+        problemPrompt: problemPrompt,
+        currentCounter: currentCounter,
+        resultMarker: resultMarker,
+        problemData: problemData,
+        currProblem: 0
+    };
+
+    // Setup problem navigation.
+    prevButton.addEventListener('click', () => {
+        navigateProblem(problemArgs, -1);
+    });
+    nextButton.addEventListener('click', () => {
+        navigateProblem(problemArgs, 1);
+    });
+
+    // Setup problem checking.
+    checkButton.addEventListener('click', () => {
+        submitAnswer(problemArgs);       
+    });
+
+    // Setup keyboard shortcuts.
+    problemArgs.problemData.forEach(data => {
+        data.answerInput.addEventListener('keydown', (e) => {
+            if (e.key == 'Enter')
+                submitAnswer(problemArgs);
+        });
+    });
+
+    // Setup the first problem.
+    problems[problemArgs.currProblem].hidden = false;
+    updateProblemUi(problemArgs);
+    totalCounter.textContent = problems.length;
+}
+
+})();
